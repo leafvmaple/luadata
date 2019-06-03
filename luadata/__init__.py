efee3b79a69dcaecbd3bbcf6dbf73045799afb85
Zohar Lee <root@leafvmaple.com>
2019-06-03 19:02:20 +0800

    Add Read Module

--- luadata/__init__.py
+++ luadata/__init__.py
@@ -1,6 +1,8 @@
-name = "luadata"
 
 from luadata.io.write import serialize
+from luadata.io.read import unserialize
+
+name = "luadata"
 
 __doc__ = """
 a Python package that can serialize Python list & dictionary to Lua table.
--- luadata/io/read.py
+++ luadata/io/read.py
@@ -1,99 +1,155 @@
 import codecs
 
 
-def read_seek(fl, offset):
-    tell = fl.tell()
-    fl.seek(tell + offset)
+class StreamData:
+    def __init__(self, data, p):
+        self.data = data
+        self.p = p
 
 
-def read_trim(fl):
-    while True:
-        ch = fl.read(1)
-        if ch != '\r' and ch != '\n' and ch != '\t' and ch != ' ':
-            read_seek(fl, -1)
-            break
+def valid(ch):
+    return ch != '\r' and ch != '\n' and ch != '\t' and ch != ' '
 
 
-def read_string(fl):
+def format_marks(string):
+    str_len = len(string)
+    if str_len > 0 and string[0] == '\"' and string[str_len - 1] == '\"':
+        return string[1:str_len - 1]
+    elif str_len > 0 and string[0] == '\'' and string[str_len - 1] == '\'':
+        return string[1:str_len - 1]
+    return string
+
+
+def format_value(val):
+    if val.isdigit():
+        return int(val)
+
+
+def read_char(info):
+    return info.data[info.p]
+
+
+def read_next(info):
+    info.p += 1
+    return read_char(info)
+
+
+def read_trim(info):
+    ch = read_char(info)
+    while not valid(ch):
+        ch = read_next(info)
+
+
+def read_chars(info):
     buffer = ""
-
-    read_trim(fl)
-    while True:
-        ch = fl.read(1)
-        if ch == '\r' or ch == '\n' or ch == ' ' or ch == '{' or ch == '}' or ch == '=' or ch == ',':
-            read_seek(fl, -1)
-            return buffer
-        else:
-            buffer += ch
+    read_trim(info)
+    ch = read_char(info)
+    while valid(ch) and ch != '{' and ch != '}' and ch != '=' and ch != ',':
+        buffer += ch
+        ch = read_next(info)
+    read_trim(info)
+    return buffer
 
 
-def read_key(fl):
-    read_trim(fl)
-    key = read_string(fl)
-    read_trim(fl)
-    str_len = len(key)
-    if str_len > 0 and key[0] == '[' and key[str_len - 1] == ']':
-        key = key[1:str_len - 1]
-
-    if key.isdigit():
-        return int(key)
-
-    return key
+def read_brackets(info):
+    buffer = ""
+    read_trim(info)
+    ch = read_next(info)
+    while ch != ']':
+        buffer += ch
+        ch = read_next(info)
+    info.p += 1
+    read_trim(info)
+    return buffer
 
 
-def read_table(fl):
+def read_string(info):
+    buffer = ""
+    read_trim(info)
+    left_ch = read_char(info)
+    ch = read_next(info)
+    while ch != left_ch:
+        buffer += ch
+        ch = read_next(info)
+    info.p += 1
+    read_trim(info)
+    return buffer
+
+
+def read_table(info):
     index = 0
     table = {}
+    lists = []
 
-    while True:
-        ch = fl.read(1)
-        if ch == "}":
-            break
-        else:
-            read_seek(fl, -1)
-
-        key = read_key(fl)
-        ch = fl.read(1)
+    ch = read_next(info)
+    while ch != '}':
+        key = read_key(info)
+        read_trim(info)
+        ch = read_char(info)
         if ch == "=":
-            table[key] = read_item(fl)
+            info.p += 1
+            table[key] = read_item(info)
         else:
-            read_seek(fl, -1)
-            table[index] = key
             index += 1
+            table[index] = format_value(key)
 
-        read_trim(fl)
-        ch = fl.read(1)
-        if ch == '}':
-            break
-        elif ch != ',':
-            print('Get Table Error')
-        read_trim(fl)
+        read_trim(info)
+        ch = read_char(info)
 
+        if ch == ',':
+            info.p += 1
+            read_trim(info)
+            ch = read_char(info)
+
+    info.p += 1
+
+    for i in range(1, len(table) + 1):
+        if i in table:
+            lists.append(table[i])
+
+    if len(lists) == len(table):
+        return lists
     return table
 
 
-def read_item(fl):
-    read_trim(fl)
-    ch = fl.read(1)
-    if ch == '{':
-        return read_table(fl)
+def read_key(info):
+    read_trim(info)
+    ch = read_char(info)
+    if ch == '\"' or ch == '\'':
+        return read_string(info)
+    elif ch == '[':
+        key = read_brackets(info)
+        if key[0] == '\"' or key[0] == '\'':
+            return format_marks(key)
+        elif key.isdigit():
+            return int(key)
+        return key
+    else:
+        return read_chars(info)
 
-    read_seek(fl, -1)
-    str = read_string(fl)
-    if str.isdigit():
-        return float(str)
-    elif str == "true":
+
+def read_item(info):
+    read_trim(info)
+    ch = read_char(info)
+    if ch == '{':
+        return read_table(info)
+    elif ch == '\"' or ch == '\'':
+        return read_string(info)
+
+    item = read_chars(info)
+    if item.isdigit():
+        return int(item)
+    elif item == "true":
         return True
-    elif str == 'false':
+    elif item == 'false':
         return False
     else:
-        return str
+        return item
 
 
 def unserialize(path, encoding='utf-8'):
     fl = codecs.open(path, 'r', encoding)
-    data = read_table(fl)
+    src_data = fl.read()
+    data = read_item(StreamData("{%s}" % src_data, 0))
     fl.close()
     return data
-
-# unserialize('.\Test.lua')
