94506e753a3b90345634c323238ff8f0c89018a1
Zohar Lee <root@leafvmaple.com>
2019-06-18 15:41:35 +0800

    Fixed the bug when the Lua table value is table

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,13 @@
 # Luadata Change Log
 
+## [0.0.6] 2019-06-18
+
+* `Fixed the bug` when the Lua table value is `table`
+
+## [0.0.5] 2019-06-03
+
+* Add `unserialize` module
+
 ## [0.0.4] 2019-04-30
 
 * `Fixed the bug` when the Python dictionary key is `None`
--- luadata/io/read.py
+++ luadata/io/read.py
@@ -6,149 +6,145 @@ class StreamData:
         self.data = data
         self.p = p
 
+    @staticmethod
+    def valid(ch):
+        return ch != '\r' and ch != '\n' and ch != '\t' and ch != ' '
 
-def valid(ch):
-    return ch != '\r' and ch != '\n' and ch != '\t' and ch != ' '
+    @staticmethod
+    def format_marks(string):
+        str_len = len(string)
+        if str_len > 0 and string[0] == '\"' and string[str_len - 1] == '\"':
+            return string[1:str_len - 1]
+        elif str_len > 0 and string[0] == '\'' and string[str_len - 1] == '\'':
+            return string[1:str_len - 1]
+        return string
 
-
-def format_marks(string):
-    str_len = len(string)
-    if str_len > 0 and string[0] == '\"' and string[str_len - 1] == '\"':
-        return string[1:str_len - 1]
-    elif str_len > 0 and string[0] == '\'' and string[str_len - 1] == '\'':
-        return string[1:str_len - 1]
-    return string
-
-
-def format_value(val):
-    if val.isdigit():
-        return int(val)
-
-
-def read_char(info):
-    return info.data[info.p]
-
-
-def read_next(info):
-    info.p += 1
-    return read_char(info)
-
-
-def read_trim(info):
-    ch = read_char(info)
-    while not valid(ch):
-        ch = read_next(info)
-
-
-def read_chars(info):
-    buffer = ""
-    read_trim(info)
-    ch = read_char(info)
-    while valid(ch) and ch != '{' and ch != '}' and ch != '=' and ch != ',':
-        buffer += ch
-        ch = read_next(info)
-    read_trim(info)
-    return buffer
-
-
-def read_brackets(info):
-    buffer = ""
-    read_trim(info)
-    ch = read_next(info)
-    while ch != ']':
-        buffer += ch
-        ch = read_next(info)
-    info.p += 1
-    read_trim(info)
-    return buffer
-
-
-def read_string(info):
-    buffer = ""
-    read_trim(info)
-    left_ch = read_char(info)
-    ch = read_next(info)
-    while ch != left_ch:
-        buffer += ch
-        ch = read_next(info)
-    info.p += 1
-    read_trim(info)
-    return buffer
-
-
-def read_table(info):
-    index = 0
-    table = {}
-    lists = []
-
-    ch = read_next(info)
-    while ch != '}':
-        key = read_key(info)
-        read_trim(info)
-        ch = read_char(info)
-        if ch == "=":
-            info.p += 1
-            table[key] = read_item(info)
-        else:
-            index += 1
-            table[index] = format_value(key)
-
-        read_trim(info)
-        ch = read_char(info)
-
-        if ch == ',':
-            info.p += 1
-            read_trim(info)
-            ch = read_char(info)
-
-    info.p += 1
-
-    for i in range(1, len(table) + 1):
-        if i in table:
-            lists.append(table[i])
-
-    if len(lists) == len(table):
-        return lists
-    return table
-
-
-def read_key(info):
-    read_trim(info)
-    ch = read_char(info)
-    if ch == '\"' or ch == '\'':
-        return read_string(info)
-    elif ch == '[':
-        key = read_brackets(info)
-        if key[0] == '\"' or key[0] == '\'':
-            return format_marks(key)
-        elif key.isdigit():
-            return int(key)
+    @staticmethod
+    def format_key(key):
+        if key[0] == '[':
+            key = key[1:len(key) - 1]
+            if key[0] == '\"' or key[0] == '\'':
+                return StreamData.format_marks(key)
+            elif key.isdigit():
+                return int(key)
         return key
-    else:
-        return read_chars(info)
 
+    @staticmethod
+    def format_value(val):
+        if isinstance(val, str) and val.isdigit():
+            return int(val)
+        return val
 
-def read_item(info):
-    read_trim(info)
-    ch = read_char(info)
-    if ch == '{':
-        return read_table(info)
-    elif ch == '\"' or ch == '\'':
-        return read_string(info)
+    def read_char(self):
+        return self.data[self.p]
 
-    item = read_chars(info)
-    if item.isdigit():
-        return int(item)
-    elif item == "true":
-        return True
-    elif item == 'false':
-        return False
-    else:
-        return item
+    def read_next(self):
+        self.p += 1
+        return self.read_char()
+
+    def read_trim(self):
+        ch = self.read_char()
+        while not StreamData.valid(ch):
+            ch = self.read_next()
+
+    def read_chars(self):
+        buffer = ""
+        self.read_trim()
+        ch = self.read_char()
+        while StreamData.valid(ch) and ch != '{' and ch != '}' and ch != '=' and ch != ',':
+            buffer += ch
+            ch = self.read_next()
+        self.read_trim()
+        return buffer
+
+    def read_brackets(self):
+        buffer = ""
+        self.read_trim()
+        ch = self.read_next()
+        while ch != ']':
+            buffer += ch
+            ch = self.read_next()
+        self.p += 1
+        self.read_trim()
+        return buffer
+
+    def read_string(self):
+        buffer = ""
+        self.read_trim()
+        left_ch = self.read_char()
+        ch = self.read_next()
+        while ch != left_ch:
+            buffer += ch
+            ch = self.read_next()
+        self.p += 1
+        self.read_trim()
+        return buffer
+
+    def read_table(self):
+        index = 0
+        table = {}
+        lists = []
+
+        ch = self.read_next()
+        while ch != '}':
+            item = self.read_item()
+            self.read_trim()
+            ch = self.read_char()
+            if ch == "=":
+                self.p += 1
+                table[StreamData.format_key(item)] = self.read_item()
+            else:
+                index += 1
+                table[index] = item
+
+            self.read_trim()
+            ch = self.read_char()
+
+            if ch == ',':
+                self.p += 1
+                self.read_trim()
+                ch = self.read_char()
+
+        self.p += 1
+
+        for i in range(1, len(table) + 1):
+            if i in table:
+                lists.append(table[i])
+
+        if len(lists) == len(table):
+            return lists
+        return table
+
+    def read_pairs(self):
+        key = self.read_item()
+        self.read_trim()
+        ch = self.read_char()
+        if ch == "=":
+            self.p += 1
+            return StreamData.format_value(key), self.read_item()
+        return key
+
+    def read_item(self):
+        self.read_trim()
+        ch = self.read_char()
+        if ch == '{':
+            return self.read_table()
+        elif ch == '\"' or ch == '\'':
+            return self.read_string()
+
+        item = self.read_chars()
+        if item.isdigit():
+            return int(item)
+        elif item == "true":
+            return True
+        elif item == 'false':
+            return False
+        else:
+            return item
 
 
 def unserialize_buffer(src_data):
-    return read_item(StreamData("{%s}" % src_data, 0))
+    return StreamData("{%s}" % src_data, 0).read_item()
 
 
 def unserialize(path, encoding='utf-8'):
