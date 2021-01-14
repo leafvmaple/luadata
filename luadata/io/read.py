dd8eca8739d89cc1531d0f3cb0146ed97e90ec44
Emil Zhai <root@derzh.com>
2021-01-14 18:32:14 +0800

    fix: auto strip return when read

--- luadata/io/read.py
+++ luadata/io/read.py
@@ -13,4 +13,14 @@ def read(path, encoding="utf-8"):
         tuple([*]): unserialized data from luadata file
     """
     with codecs.open(path, "r", encoding) as file:
+        text = file.read().strip()
+        if text[0:6] == "return":
+            ch = text[6:7]
+            if not (
+                (ch >= "a" and ch <= "z")
+                or (ch >= "A" and ch <= "Z")
+                or (ch >= "0" and ch <= "9")
+                or ch == "_"
+            ):
+                text = text[6:]
         return unserialize(file.read())
